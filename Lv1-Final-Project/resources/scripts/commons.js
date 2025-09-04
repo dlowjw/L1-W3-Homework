e77c9ee23a936ade17e6df1c0266e51fe3530113
dlowjw <dlowjw@gmail.com>
2025-09-04 15:56:18 +0800

    Mobile button added to all pages and mobile menu implemented

--- Lv1-Final-Project/resources/scripts/commons.js
+++ Lv1-Final-Project/resources/scripts/commons.js
@@ -1,13 +1,19 @@
 const themeBtn = document.getElementsByClassName("theme-button");
 const body = document.getElementsByClassName("body");
 
+const mobileMenuBtn = document.getElementById("mobile-menu-button");
+const mobileMenu = document.getElementById("mobile-menu");
+
 let isLight = false;
 const themePref = localStorage.getItem("theme");
 if (themePref === "light" && !body[0].classList.contains("light"))  {
   toggleTheme();
 }
 
+console.log(mobileMenuBtn);
+
 themeBtn[0].addEventListener("click", toggleTheme);
+mobileMenuBtn.addEventListener("click", toggleMobileMenu);
 
 function toggleTheme() {
   body[0].classList.toggle("light");
@@ -21,4 +27,8 @@ function toggleTheme() {
     themeBtn[0].textContent = "🌙";
     localStorage.setItem("theme", "dark");
   }
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("visible");
 }
